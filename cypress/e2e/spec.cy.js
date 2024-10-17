596ee891dfeede49054d90460799fcf65c2efc1c
EzinneOkor <ezinnejohnndukwe5@gmail.com>
2024-10-17 22:25:10 +0100

    Opening Website

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,11 @@
+describe('start testing',() => {
+  it('open web', ()=>{
+    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+  })
+})
+
+// describe('template spec', () => {
+//   it('passes', () => {
+//     cy.visit('https://example.cypress.io')
+//   })
+// })
